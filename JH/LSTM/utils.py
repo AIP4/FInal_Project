5df251ad230f05af8877f183e78e72200aec4aeb
opkwisdom <datah07301@gmail.com>
2024-12-06 07:41:27 +0900

    CNN-LSTM complete

--- JH/LSTM/utils.py
+++ JH/LSTM/utils.py
@@ -41,7 +41,7 @@ def print_missing_info(df):
     print(non_zero_missing)
     print(df.columns)
 
-def convert_timesteps(data):
+def convert_timesteps(data, diff=3):
     """
     datetime 형식으로 변환
     """
@@ -54,7 +54,7 @@ def convert_timesteps(data):
 
     # diff 재계산
     data['diff'] = data['TM'].diff().dt.total_seconds() // 3600
-    data.loc[0, 'diff'] = 3
+    data.loc[0, 'diff'] = diff
 
     return data
 
@@ -86,3 +86,58 @@ def prepare_data(region, columns_to_remove, include_china=False):
     df = filter_columns(df, columns_to_remove)
     df = convert_timesteps(df)
     return df
+
+def china_pm_data():
+    """
+    중국 데이터 파일을 읽어와서 PM2.5만 반환
+    """
+    cma_locations = ["yanan", "qingdao", "chifeng", "dalian"]
+
+    df = pd.DataFrame()
+    for loc in cma_locations:
+        cma_path = f"../../collect_data/filtered/cma/{loc}_meta.csv"
+        df_cma = pd.read_csv(cma_path)
+        
+        df_cma.rename(columns={"PM10": f"PM10_{loc}"}, inplace=True)
+        df = pd.concat([df, df_cma[f"PM10_{loc}"]], axis=1)
+
+    df = pd.concat([df, df_cma['TM']], axis=1)
+    df.insert(0, 'TM', df.pop('TM'))
+
+    return df
+
+
+def imputed_china_pm_data():
+    df = china_pm_data()
+
+    # Remove the null rows
+    df = df.loc[:10306]
+    time_column = df["TM"]
+
+    pm_columns = df.columns[1:]
+    df[pm_columns] = df[pm_columns].applymap(lambda x: np.nan if x <= 0 else x)
+
+    df['TM'] = pd.to_datetime(df['TM'], format='%Y%m%d%H%M')
+    df.set_index('TM', inplace=True)
+
+    # 시간 기반 선형 보간법 적용
+    df.interpolate(method='time', inplace=True)
+    
+    df.reset_index(inplace=True)
+    df["TM"] = time_column
+
+    return df
+
+def prepare_data_for_CNN_LSTM(region, columns_to_remove):
+    filename = f"../../collect_data/filtered/kma/merged/kma_{region}_meta.csv"
+    df = pd.read_csv(filename)
+    df_cma = imputed_china_pm_data()
+    df = pd.merge(df, df_cma, on="TM", how="inner")
+    df = filter_columns(df, columns_to_remove)
+    df = convert_timesteps(df, diff=3)
+
+    return df
+
+def get_model_size(model):
+    total_params = sum(p.numel() for p in model.parameters())
+    return total_params
